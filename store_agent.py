b8259724b1b9ca86fe556f4ae06633fac63dbf91
Varshitha-M-ai <varshitha.m694@gmail.com>
2025-04-07 20:16:17 +0530

    remove

--- store_agent.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def get_store_alerts():
-    df = pd.read_csv("data/inventory_monitoring.csv")
-    df_alerts = df[df["Stock Levels"] < df["Reorder Point"]]
-    return df_alerts[["Product ID", "Store ID", "Stock Levels", "Reorder Point"]].to_dict(orient="records")
